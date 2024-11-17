b247ea2a679def79463a45acb0975f89909732e5
Siva Reddy <sivadeveloper02@gmail.com>
2024-11-17 07:42:39 +0000

    Removed unnecessary space

--- simple_interest.py
+++ simple_interest.py
@@ -1,6 +1,5 @@
 principal_amount = int(input("Enter the princepal amount: "))
 time = int(input("Enter the time period in years: "))
 rate_of_interest = float(input("Enter the rate of interest: "))
-
-simple_interst = principal_amount * time * rate_of_interest / 100
-print(simple_interst)
+simple_interest = principal_amount * time * rate_of_interest / 100
+print(simple_interest)
