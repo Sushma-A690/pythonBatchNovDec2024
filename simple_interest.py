90dc76de880d490b97ade0d9fd2d94b810e7d592
Siva Reddy <sivadeveloper02@gmail.com>
2024-11-17 07:23:16 +0000

    simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -4,5 +4,3 @@ rate_of_interest = float(input("Enter the rate of interest: "))
 
 simple_interst = principal_amount * time * rate_of_interest / 100
 print(simple_interst)
-componed_interst =  simple_interst-principal_amount
-print(componed_interst)
